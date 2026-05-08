b1ab70cc8fb41c5f3977dc78fd1e6a30a4733e7b
sh00von <u2110015@student.cuet.ac.bd>
2026-05-08 15:32:05 +0600

    Use correct Sanity project id

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "8427m7c1";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "h6kdvnxs";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineCliConfig({
