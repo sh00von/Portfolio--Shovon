7821dc8a9a4318199a039dce491ec75828792824
sh00von <u2110015@student.cuet.ac.bd>
2026-05-08 15:24:39 +0600

    Fix Sanity studio loading

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,6 +1,6 @@
 export const apiVersion = "2026-05-08";
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "8427m7c1";
 
 export const hasSanityConfig = Boolean(projectId && dataset);
