6fdfcb41038453a05c406ddeaef778627136253c
sh00von <u2110015@student.cuet.ac.bd>
2026-07-22 21:29:56 +0600

    chore: remove Google Analytics and Microsoft Clarity tracking scripts completely

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from "next";
 import { Mulish } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const mulish = Mulish({
@@ -73,27 +72,6 @@ export default function RootLayout({
         />
       </head>
       <body className={`${mulish.className} min-h-screen`}>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-JJF34HQPL9"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JJF34HQPL9');
-          `}
-        </Script>
-        <Script id="clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "wbdj1lhmzk");
-          `}
-        </Script>
         {children}
       </body>
     </html>
